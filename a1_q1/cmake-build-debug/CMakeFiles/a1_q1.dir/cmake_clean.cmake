2def82350527b5f788cb3a1f6cbff18b7560ca30
caydendevelop <caydendevelop@gmail.com>
2020-10-08 15:45:10 +0800

    Initial commit

---
+++ a1_q1/cmake-build-debug/CMakeFiles/a1_q1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a1_q1.dir/main.c.o"
+  "a1_q1"
+  "a1_q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a1_q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ a1_q2/cmake-build-debug/CMakeFiles/a1_q2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for a1_q2.
+# This may be replaced when dependencies are built.
