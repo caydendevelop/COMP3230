3324fbcafd37f99e60c91c7c33096b89d4fdd3a6
caydendevelop <caydendevelop@gmail.com>
2020-10-15 18:52:26 +0800

    Update main.c

--- a1_q2_2/main.c
+++ a1_q2_2/main.c
@@ -35,20 +35,20 @@ int main(int argc, char* argv[])
     // sleep(1);  // This line is only for test. Remove this line when you implement your solution
 
     //mergesort4Way4Processes(pInputArray, 0, num_integers);
-    recursiveMergesort(pInputArray, 0, num_integers, num_integers, 0, n);
+    recursiveMergesort(pInputArray, 0, num_integers, num_integers);
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     printf("End timing.\n");
     uint64_t delta_ms = (end.tv_sec - start.tv_sec) * 1.0e3 + (end.tv_nsec - start.tv_nsec) * 1.0e-6;
     printf("The elapsed time (ms) is %lu \n", delta_ms);
-    // uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1.0e6 + (end.tv_nsec - start.tv_nsec) * 1.0e-3;
-    // printf("The elapsed time (us) is %lu \n", delta_us);
-    // uint64_t delta_s = (end.tv_sec - start.tv_sec);
-    // printf("The elapsed time (s) is %lu \n", delta_s);
+     uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1.0e6 + (end.tv_nsec - start.tv_nsec) * 1.0e-3;
+     printf("The elapsed time (us) is %lu \n", delta_us);
+     uint64_t delta_s = (end.tv_sec - start.tv_sec);
+     printf("The elapsed time (s) is %lu \n", delta_s);
 
 
     bubble_sort(pInputArray, num_integers);
-    //verifySortResults(pInputArray, YOUR_ARRAY, num_integers); // Replace YOUR_ARRAY by your array name
+    verifySortResults(pInputArray, pInputArray, num_integers); // Replace YOUR_ARRAY by your array name
 
     free(pInputArray);
     printf("This is the END of the program.\n");
